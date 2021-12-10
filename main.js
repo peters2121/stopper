9f3bed5d5505dd12be2aaffed004b9435abc8363
Morakinyo Peters <oluwaseyi93@gmail.com>
2021-12-10 17:05:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var timer = 0;
+var timerInterval;
+var ms= document.getElementById("milliseconds");
+var second = document.getElementById("mainsecond");
+var minute = document.getElementById("mainminute");
+
+function start(){
+    stop();
+    timerInterval= setInterval(function(){
+        timer += 1/60;
+        msVal = Math.floor((timer - Math.floor(timer))*100);
+        secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+        minuteVal = Math.floor(timer/60);
+        ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
+        second.innerHTML = secondVal < 10 ? "0" + secondVal.toString(): secondVal;
+        minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString(): minuteVal;
+    }, 1000/60)
+}
+
+function stop(){
+    clearInterval(timerInterval)
+}
+
+function reset(){
+    timer = 0;
+    stop();
+    second.innerHTML = "00";
+    minute.innerHTML = "00";
+    ms.innerHTML = "00";
+}
